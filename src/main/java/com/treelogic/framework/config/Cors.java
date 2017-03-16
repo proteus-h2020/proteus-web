6075796d313ad4669aec969b1657385f0679c11d
0xNacho <nachogarcia91@gmail.com>
2017-03-16 15:17:24 +0100

    Add models and repositories

---
+++ src/main/java/com/treelogic/framework/config/Cors.java
@@ -0,0 +1,12 @@
+package com.treelogic.framework.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Configuration
+@CrossOrigin(origins = {"*"})
+public class Cors {
+
+	
+	
+}
---
+++ src/main/java/com/treelogic/framework/domain/User.java
@@ -0,0 +1,99 @@
+package com.treelogic.framework.domain;
+
+import javax.validation.constraints.NotNull;
+import org.springframework.data.mongodb.core.mapping.Document;
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.treelogic.framework.advice.ExcludeFieldsAdvice;
+
+@Document
+@JsonFilter(ExcludeFieldsAdvice.FILTER_NAME)
+public class User  {
+
+	@NotNull
+	private String email;
+
+	@NotNull
+	private String password;
+
+	@NotNull
+	private String name;
+	
+	@NotNull
+	private String lastname;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((lastname == null) ? 0 : lastname.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (lastname == null) {
+			if (other.lastname != null)
+				return false;
+		} else if (!lastname.equals(other.lastname))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		return true;
+	}
+	
+}
