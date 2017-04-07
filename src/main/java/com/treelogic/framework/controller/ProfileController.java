4d6c5c7f70c11f2718fef0fb277bab91aa39ec82
Jorge Yagüe París <jorge.yague@treelogic.com>
2017-04-07 08:39:14 +0200

    Add user profiles

---
+++ src/main/java/com/treelogic/framework/controller/ProfileController.java
@@ -0,0 +1,29 @@
+package com.treelogic.framework.controller;
+
+import com.treelogic.framework.domain.Profile;
+import com.treelogic.framework.service.ProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/profile")
+public class ProfileController {
+
+    private final ProfileService profileService;
+
+    @Autowired
+    public ProfileController(ProfileService profileService) {
+        this.profileService = profileService;
+    }
+
+    @RequestMapping(
+        path = "/{username}",
+        method = RequestMethod.GET
+    )
+    public Profile profile(@PathVariable String username) throws Exception {
+        return profileService.getProfile(username).orElseThrow(() -> new Exception("User not found: " + username));
+    }
+}
--- src/main/java/com/treelogic/framework/domain/User.java
+++ src/main/java/com/treelogic/framework/domain/User.java
@@ -10,23 +10,49 @@ import com.treelogic.framework.advice.ExcludeFieldsAdvice;
 public class User  {
 
 	@NotNull
-	private String email;
+	private String username;
 
 	@NotNull
 	private String password;
 
 	@NotNull
-	private String name;
-	
-	@NotNull
-	private String lastname;
+	private String email;
 
-	public String getEmail() {
-		return email;
+	private String fullName;
+
+	@NotNull
+	private String alias;
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		User user = (User) o;
+
+		if (!username.equals(user.username)) return false;
+		if (!password.equals(user.password)) return false;
+		if (!email.equals(user.email)) return false;
+		if (fullName != null ? !fullName.equals(user.fullName) : user.fullName != null) return false;
+		return alias.equals(user.alias);
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
+	@Override
+	public int hashCode() {
+		int result = username.hashCode();
+		result = 31 * result + password.hashCode();
+		result = 31 * result + email.hashCode();
+		result = 31 * result + (fullName != null ? fullName.hashCode() : 0);
+		result = 31 * result + alias.hashCode();
+		return result;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
 	}
 
 	public String getPassword() {
@@ -37,63 +63,112 @@ public class User  {
 		this.password = password;
 	}
 
-	public String getName() {
-		return name;
+	public String getEmail() {
+		return email;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
-	public String getLastname() {
-		return lastname;
+	public String getFullName() {
+		return fullName;
 	}
 
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = super.hashCode();
-		result = prime * result + ((email == null) ? 0 : email.hashCode());
-		result = prime * result + ((lastname == null) ? 0 : lastname.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((password == null) ? 0 : password.hashCode());
-		return result;
+	public String getAlias() {
+		return alias;
 	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (!super.equals(obj))
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		if (email == null) {
-			if (other.email != null)
-				return false;
-		} else if (!email.equals(other.email))
-			return false;
-		if (lastname == null) {
-			if (other.lastname != null)
-				return false;
-		} else if (!lastname.equals(other.lastname))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (password == null) {
-			if (other.password != null)
-				return false;
-		} else if (!password.equals(other.password))
-			return false;
-		return true;
+	public void setAlias(String alias) {
+		this.alias = alias;
 	}
+
+	//	@NotNull
+//	private String password;
+//
+//	@NotNull
+//	private String name;
+//
+//	@NotNull
+//	private String lastname;
+
+//	public String getEmail() {
+//		return email;
+//	}
+//
+//	public void setEmail(String email) {
+//		this.email = email;
+//	}
+//
+//	public String getPassword() {
+//		return password;
+//	}
+//
+//	public void setPassword(String password) {
+//		this.password = password;
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	public String getLastname() {
+//		return lastname;
+//	}
+//
+//	public void setLastname(String lastname) {
+//		this.lastname = lastname;
+//	}
+//
+//	@Override
+//	public int hashCode() {
+//		final int prime = 31;
+//		int result = super.hashCode();
+//		result = prime * result + ((email == null) ? 0 : email.hashCode());
+//		result = prime * result + ((lastname == null) ? 0 : lastname.hashCode());
+//		result = prime * result + ((name == null) ? 0 : name.hashCode());
+//		result = prime * result + ((password == null) ? 0 : password.hashCode());
+//		return result;
+//	}
+//
+//	@Override
+//	public boolean equals(Object obj) {
+//		if (this == obj)
+//			return true;
+//		if (!super.equals(obj))
+//			return false;
+//		if (getClass() != obj.getClass())
+//			return false;
+//		User other = (User) obj;
+//		if (email == null) {
+//			if (other.email != null)
+//				return false;
+//		} else if (!email.equals(other.email))
+//			return false;
+//		if (lastname == null) {
+//			if (other.lastname != null)
+//				return false;
+//		} else if (!lastname.equals(other.lastname))
+//			return false;
+//		if (name == null) {
+//			if (other.name != null)
+//				return false;
+//		} else if (!name.equals(other.name))
+//			return false;
+//		if (password == null) {
+//			if (other.password != null)
+//				return false;
+//		} else if (!password.equals(other.password))
+//			return false;
+//		return true;
+//	}
 	
 }
