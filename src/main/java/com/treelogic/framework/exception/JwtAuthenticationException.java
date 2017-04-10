1332deb44924bf67d75e26336b559f17fabf26cc
0xNacho <nachogarcia91@gmail.com>
2017-04-10 15:39:49 +0200

    Fix error handler  class on JWTAuthenticationException

--- src/main/java/com/treelogic/framework/exception/JwtAuthenticationException.java
+++ src/main/java/com/treelogic/framework/exception/JwtAuthenticationException.java
@@ -1,7 +1,8 @@
 package com.treelogic.framework.exception;
 
+import org.springframework.security.core.AuthenticationException;
 
-public class JwtAuthenticationException extends RuntimeException {
+public class JwtAuthenticationException extends AuthenticationException {
     public JwtAuthenticationException(String msg, Throwable t) {
         super(msg, t);
     }
