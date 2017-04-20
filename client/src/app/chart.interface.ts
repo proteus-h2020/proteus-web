ca4ef17b3d9151fc8765c78efaf0c355c23559f0
0xNacho <nachogarcia91@gmail.com>
2017-04-20 16:15:05 +0200

    Second commit

--- client/src/app/chart.interface.ts
+++
@@ -1,8 +0,0 @@
-import { WebsocketDatasource } from 'proteic';
-export interface Chart{
-    title: string;
-    type : string;
-    conf : any;
-    websocketEndpoint ? : WebsocketDatasource;
-    data ? : Array<any>;
-}
