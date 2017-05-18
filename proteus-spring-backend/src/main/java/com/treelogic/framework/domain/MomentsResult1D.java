e6fe339a571a933b4d40db01db3a3de2f05d0e33
0xNacho <nachogarcia91@gmail.com>
2017-05-18 21:37:26 +0200

    Add moments pojos and remove unused classes

---
+++ proteus-spring-backend/src/main/java/com/treelogic/framework/domain/MomentsResult1D.java
@@ -0,0 +1,44 @@
+package com.treelogic.framework.domain;
+
+public class MomentsResult1D extends MomentsResult {
+
+	private double x;
+	
+	public MomentsResult1D(int coilId, int varId, double mean, double variance, double counter, double x){
+		this.coilId = coilId;
+		this.varId = varId;
+		this.mean = mean;
+		this.variance = variance;
+		this.counter = counter;
+		this.x = x;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		long temp;
+		temp = Double.doubleToLongBits(x);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MomentsResult1D other = (MomentsResult1D) obj;
+		if (Double.doubleToLongBits(x) != Double.doubleToLongBits(other.x))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
