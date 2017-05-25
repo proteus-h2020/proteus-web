072168a0d92412c4887f9ee30467d94fbbd09bc1
Jorge Yagüe París <jorge.yague@treelogic.com>
2017-05-25 10:09:36 +0200

    Remove unused page routes

--- proteus-spring-dashboard/src/app/pages/pages.routing.ts
+++ proteus-spring-dashboard/src/app/pages/pages.routing.ts
@@ -20,16 +20,9 @@ export const routes: Routes = [
     children: [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
-      { path: 'editors', loadChildren: './editors/editors.module#EditorsModule' },
       { path: 'visualizations', loadChildren: './visualizations/visualizations.module#VisualizationsModule' },
-      { path: 'components', loadChildren: './components/components.module#ComponentsModule' },
-      { path: 'charts', loadChildren: './charts/charts.module#ChartsModule' },
-      { path: 'ui', loadChildren: './ui/ui.module#UiModule' },
-      { path: 'forms', loadChildren: './forms/forms.module#FormsModule' },
-      { path: 'tables', loadChildren: './tables/tables.module#TablesModule' },
-      { path: 'maps', loadChildren: './maps/maps.module#MapsModule' }
-    ]
-  }
+    ],
+  },
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forChild(routes);
