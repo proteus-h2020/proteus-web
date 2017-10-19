bb49303fc9986da8df2f4e1200cfd90117e8a3e1
gyeongchan yun <gyeongchan.yun@treelogic.com>
2017-10-19 09:48:54 +0200

    Modify new-visualization configuration menu and style

--- proteus-spring-dashboard/src/app/pages/visualizations/keys.pipe.ts
+++ proteus-spring-dashboard/src/app/pages/visualizations/keys.pipe.ts
@@ -5,8 +5,8 @@ export class KeysPipe implements PipeTransform {
   transform(value, args: string[]): any {
     let keys = [];
     for (let key in value) {
-      // only allow to modify string keys
-      if (typeof value[key] === 'string') {
+      // only allow to modify string and boolean keys
+      if (typeof value[key] === 'string' || typeof value[key] === 'boolean') {
         keys.push({ key: key, value: value[key] });
       }
     }
