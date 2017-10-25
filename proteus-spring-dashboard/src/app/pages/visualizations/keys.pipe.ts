64f193f8e52254b6ebee72d26a10eeeb5fd6d5ea
gyeongchan-yun <rugyoon@gmail.com>
2017-10-25 17:37:02 +0200

    Add no-serve keys on configuration menu

--- proteus-spring-dashboard/src/app/pages/visualizations/keys.pipe.ts
+++ proteus-spring-dashboard/src/app/pages/visualizations/keys.pipe.ts
@@ -4,10 +4,11 @@ import { PipeTransform, Pipe } from '@angular/core';
 export class KeysPipe implements PipeTransform {
   transform(value, args: string[]): any {
     let keys = [];
-    let noServeKeys = ['selector', 'legendPosition', 'pauseButtonPosition']; // The property which user can't set
+    // The property which can't be shown on configuration menu irrespective of condition
+    let noServeKeys = ['selector', 'width', 'legendPosition', 'pauseButtonPosition'];
     for (let key in value) {
-      // only allow to modify not css property (usually type of css property is number)
       if (noServeKeys.indexOf(key) == -1) {
+          // only allow to modify non-css property (usually type of css property is number)
           if (typeof value[key] === 'string' || typeof value[key] === 'boolean') {
             keys.push({ key: key, value: value[key] });
           }
