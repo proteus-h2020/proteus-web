b1f8dd6c9e0b986189d3634bf15ba43557ab8828
gyeongchan-yun <rugyoon@gmail.com>
2017-12-05 12:55:03 +0100

    Lint code style and add ngOnInit and ngOnDestory import

--- proteus-spring-dashboard/src/app/notifications.service.ts
+++ proteus-spring-dashboard/src/app/notifications.service.ts
@@ -1,22 +1,22 @@
 import { Injectable } from '@angular/core';
-import {Subject} from 'rxjs/Subject';
+import { Subject } from 'rxjs/Subject';
 
 @Injectable()
 export class NotificationsService {
 
-    private nofitications : Array<any> = new Array<any>();
-    private notificator : Subject<any> = new Subject<any>();
+    private nofitications: Array<any> = new Array<any>();
+    private notificator: Subject<any> = new Subject<any>();
 
     public get() {
         return this.notificator;
     }
 
-    public push(alert : any){
+    public push(alert: any) {
         this.nofitications.push(alert);
         this.notificator.next(this.nofitications);
     }
 
-    public clear(){
+    public clear() {
         this.nofitications = new Array<any>();
         this.notificator.next(this.nofitications);
     }
