e7ffbb18eee2fad44d714a5e3a6be4d6a3e13151
gyeongchan-yun <rugyoon@gmail.com>
2017-12-22 14:27:09 +0100

    Add edit and remove chart function in historical

--- proteus-spring-dashboard/src/app/pages/historical/historical.component.ts
+++ proteus-spring-dashboard/src/app/pages/historical/historical.component.ts
@@ -1,8 +1,12 @@
 import { Subscription } from 'rxjs/Rx';
+
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { ChartService } from './../../chart.service';
 import { RealtimeChart } from './../../realtime-chart';
-import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AppSubscriptionsService } from './../../appSubscriptions.service';
+
 
 @Component({
   selector: 'component-historical',
@@ -11,51 +15,60 @@ import { AppSubscriptionsService } from './../../appSubscriptions.service';
 })
 export class HistoricalComponent implements OnInit, OnDestroy {
 
+  private coilData: any;
+  private coilId: number = 40304075;
+  private varId: number = 2;
 
-  private coilData: any; 
-  private coilId: number = 40101001;
-  private varId: number = 4;
   private charts: Array<RealtimeChart> = new Array<RealtimeChart>();
   private subscriptions: Subscription[] = new Array<Subscription>();
-  
+
   constructor(
     private appSubscriptionsService: AppSubscriptionsService,
     private chartProteicService: ChartService,
+    private router: Router,
   ) {}
-  
 
- 
   ngOnInit() {
-    const coilDataSubscription = this.appSubscriptionsService.historicalData().subscribe(
-      (coilData: any) => {
-        this.coilData = coilData;
-        console.log(coilData);
-      },
-    ); 
-
-    const chartsSubscription = this.chartProteicService
-    .getChartsSubscription()
-    .subscribe(
-     (charts: RealtimeChart[]) => this.charts = charts.filter((c: RealtimeChart) => c.coilID !== 'current' ) //todo: filter with rxjs
-    );
-
-    this.subscriptions.push(coilDataSubscription);
-    this.subscriptions.push(chartsSubscription);
-    
-    this._requestHistoricalData(this.coilId, this.varId);
-    
-  }
-
-
-  private _requestHistoricalData(coilId: number, varId: number){
-    console.log('requesting historical data for coil', coilId);
-    this.appSubscriptionsService.requestHistoricalData(coilId, varId);
+    this._initializeSubscriptions();
   }
 
   ngOnDestroy() {
-    for(const s of this.subscriptions){
+    for (const s of this.subscriptions) {
       s.unsubscribe();
-    } 
+    }
+  }
+
+  removeChart(chart: RealtimeChart) {
+    this.chartProteicService.remove(chart);
+  }
+
+  editChart(chart: RealtimeChart) {
+   const chartId = chart.id;
+   this.router.navigate([`pages/visualizations/edit/${chartId}`]);
+  }
+
+  private _initializeSubscriptions() {
+    this._requestHistoricalData(this.coilId, this.varId);
+
+    const coilDataSubscription = this.appSubscriptionsService.historicalData().subscribe(
+      (coilData: any) => {
+        this.coilData = coilData;
+      },
+    );
+
+    const chartsSubscription = this.chartProteicService.getChartsSubscription().subscribe(
+      // todo: filter with rxjs
+     (charts: RealtimeChart[]) => this.charts = charts.filter((c: RealtimeChart) => c.coilID !== 'current'),
+    );
+
+    this.subscriptions.push(
+      coilDataSubscription,
+      chartsSubscription,
+    );
+  }
+
+  private _requestHistoricalData(coilId: number, varId: number) {
+    this.appSubscriptionsService.requestHistoricalData(coilId, varId);
   }
 
   /**
